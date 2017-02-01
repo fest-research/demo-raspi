8c6bcef7f8ad56a081ac33386e99c6c944f4f2f4
Christoph Held <christoph.held@est.fujitsu.com>
2017-02-01 18:29:14 +0100

    clean up

--- src/packingdesk.py
+++ src/packingdesk.py
@@ -2,11 +2,12 @@
 import RPi.GPIO as GPIO
 import time
 import urllib2
+import random
 
+cloudUrl = "http://104.155.11.172:8080/api/v1/proxy/namespaces/default/services/demo/push/"
 
 GPIO.setmode(GPIO.BCM)
 rot = 0; gruen = 1; taster=2
-
 Desk=[18,23,25]
 GPIO.setup(Desk[rot], GPIO.OUT, initial=False)
 GPIO.setup(Desk[gruen], GPIO.OUT, initial=False)
@@ -14,27 +15,22 @@ GPIO.setup(Desk[taster], GPIO.IN)
 print("starting")
 
 
-def measureTemp():
-    file = open(temp_sensor)
-    filecontent = file.read()
-    file.close()
-    stringvalue = filecontent.split("\n")[1].split(" ")[9]
-    temperature = float(stringvalue[2:]) / 1000
-    return(temperature)
-
-
-
 
 try:
 	while True:
 		if GPIO.input(Desk[taster])==True:
 			GPIO.output(Desk[rot], True)
-			time.sleep(10)			
+			time.sleep(10)
 			GPIO.output(Desk[gruen], True); GPIO.output(Desk[rot], False)
 			time.sleep(2)
 			GPIO.output(Desk[gruen], False)
-			urllib2.urlopen("http://www.heise.de").read()
+			orderId = random.randint(1000000,9000000)
+			message = "Order%20"+str(orderId)+"%20shipped"
+			urllib2.urlopen(cloudUrl+message).read()
+
+
 except KeyboardInterrupt:
 	GPIO.cleanup()
+
 
 
